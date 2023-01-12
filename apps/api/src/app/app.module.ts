92877b33a77544e54d250884b1fe185590ef181a
LouisSung <sjf.csie@gmail.com>
2023-01-12 23:25:11 +0800

    chore(api/app): support serving Angular webpage

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({ rootPath: path.join(__dirname, '../ecommerce') }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
