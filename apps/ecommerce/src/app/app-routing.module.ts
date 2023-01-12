85c77bcfc3445acabbe9e0ecdd9786d3be8037f8
LouisSung <sjf.csie@gmail.com>
2023-01-12 23:13:06 +0800

    init(app/home): init home component and app routing

---
+++ apps/ecommerce/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes, { enableTracing: false })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- apps/ecommerce/src/app/app.module.ts
+++ apps/ecommerce/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeModule } from './home/home.module';
+
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [AppRoutingModule, BrowserModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
