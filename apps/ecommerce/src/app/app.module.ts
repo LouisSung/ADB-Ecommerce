6ea38176b7e1eec6f940ad3dbbddc833454817dd
LouisSung <sjf.csie@gmail.com>
2023-01-17 08:50:36 +0800

    feat(app/layout): adopt ng-zorro for better layout

--- apps/ecommerce/src/app/app.module.ts
+++ apps/ecommerce/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -11,8 +15,13 @@ import { WordCloudModule } from './word-cloud/word-cloud.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [AppRoutingModule, BrowserModule, HttpClientModule, GraphModule, HomeModule, WordCloudModule],
+  imports: [
+    AppRoutingModule, BrowserModule, BrowserAnimationsModule, FormsModule,
+    HttpClientModule, GraphModule, HomeModule, WordCloudModule,
+    NzLayoutModule, NzMenuModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
